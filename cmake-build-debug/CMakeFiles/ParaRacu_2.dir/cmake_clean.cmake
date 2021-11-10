b3a14586cda0d31d8df95a3f6a8e5f06b8011ee9
Luxha00 <73116082+Luxha00@users.noreply.github.com>
2021-11-10 10:22:42 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ParaRacu_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParaRacu_2.dir/main.cpp.obj"
+  "ParaRacu_2.exe"
+  "ParaRacu_2.exe.manifest"
+  "ParaRacu_2.pdb"
+  "libParaRacu_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParaRacu_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

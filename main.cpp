b70c2ed910604aec4a0abb4d04a8c7a32b2d2729
Luka Hauko <73116082+Luxha00@users.noreply.github.com>
2021-11-17 14:54:21 +0100

    Update main.cpp
    ck dela

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int PSL = 0;
 double MF = 0;
-int L = 6;
+int L = 3;
 int nfesLmt = 1000000;
 vector <int> sequence;
 vector <vector<int>> sosedi;
@@ -15,10 +15,7 @@ vector <int> bestMFSequence;
 void sequenceFill(){
     sequence.push_back(1);
     sequence.push_back(-1);
-    sequence.push_back(-1);
-    sequence.push_back(-1);
     sequence.push_back(1);
-    sequence.push_back(-1);
 }
 
 void getSosede(){
@@ -47,37 +44,32 @@ void getMF(vector <int> sequence){
 
 void Ck(vector <int> sequence){
     vector <int> Cks;
-    int i = 0;
-    int l = L;
-    int Ck = 0;
-    while (i < l){
-        for (int j = 0; j < l; j++){
-            Ck += sequence[j] + sequence[j+1];
+    int ck = 0;
+    for (int k = 0; k < L - 1; k++){
+        for(int i = 0; i < L-k; i++){
+            ck += sequence[i] * sequence[i+k];
         }
-        Cks.push_back(Ck);
-        if (Ck < PSL){
-            PSL = Ck;
-            bestPSLSequence = sequence;
-        }
-        Ck = 0;
-        l--;
+        cout << ck << "; ";
+        ck = 0;
     }
     getMF(sequence);
 }
 
 int main() {
     sequenceFill();
-    vector <double> cks;
-    int temp = 0;
-    int temp2 = 0;
-    for (int i = 0; i < L - 1 ; i++){
-        temp2 = sequence[i] * sequence[i+1];
-        temp += temp2;
-        cks.push_back(temp2);
-    }
-
-    for (int i = 0; i < L - 1; i++){
-        cout << cks[i] << ";";
-    }
+    Ck(sequence);
+    //sequenceFill();
+    //vector <double> cks;
+    //int temp = 0;
+    //int temp2 = 0;
+    //for (int i = 0; i < L - 1 ; i++){
+    //    temp2 = sequence[i] * sequence[i+1];
+    //    temp += temp2;
+    //    cks.push_back(temp2);
+    //}
+    //
+    //for (int i = 0; i < L - 1; i++){
+    //    cout << cks[i] << ";";
+    //}
     return 0;
 }

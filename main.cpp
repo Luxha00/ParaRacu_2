0dc3cedec8fe4e4605b98b4428df5a6d5d37b82f
Luxha00 <73116082+Luxha00@users.noreply.github.com>
2021-11-17 20:07:12 +0100

    dsadsadsaa

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-int PSL = 0;
+int PSL = 9999;
 double MF = 0;
-int L = 3;
+int L = 8;
 int nfesLmt = 1000000;
 vector <int> sequence;
 vector <vector<int>> sosedi;
@@ -14,6 +14,11 @@ vector <int> bestMFSequence;
 
 void sequenceFill(){
     sequence.push_back(1);
+    sequence.push_back(-1);
+    sequence.push_back(-1);
+    sequence.push_back(-1);
+    sequence.push_back(1);
+    sequence.push_back(-1);
     sequence.push_back(-1);
     sequence.push_back(1);
 }
@@ -30,29 +35,52 @@ void getSosede(){
 }
 
 void getRandomSequence(int L){
-
     for ( int i = 0; i < L; i++){
         sosedi.push_back(sequence);
     }
     getSosede();
 }
 
-void getMF(vector <int> sequence){
-    int L2 = L * L;
-
+void getMF(vector <int> sequence, double ckVsota){
+    double L2 = L * L;
+    double multi = 2;
+    double mf = L2/(multi*ckVsota);
+    if (mf > MF){
+        MF = mf;
+        bestMFSequence = sequence;
+    }
+    cout << "\n" <<"best MF: " << mf;
 }
 
 void Ck(vector <int> sequence){
     vector <int> Cks;
     int ck = 0;
-    for (int k = 0; k < L - 1; k++){
+    int ck2 = 0;
+    double ckVsota = 0;
+    int ckHigh = 0;
+    for (int k = 1; k < L - 1; k++){
         for(int i = 0; i < L-k; i++){
             ck += sequence[i] * sequence[i+k];
         }
         cout << ck << "; ";
+        if (ck <0){
+            ck = -ck;
+        }
+        if (k > 1){
+            if (ck > ckHigh){
+                ckHigh = ck;
+            }
+        }
+        ck2 = ck * ck;
+        ckVsota += ck2;
         ck = 0;
     }
-    getMF(sequence);
+    if (ckHigh < PSL){
+        PSL = ckHigh;
+        bestPSLSequence = sequence;
+    }
+    getMF(sequence, ckVsota);
+    cout << "\n" << "best PSL: " << PSL;
 }
 
 int main() {
